ee1bcaaa5fc9b00fd00f05530d95853b0424f92c
Preethi-BM <preethibm12@gmail.com>
2024-04-22 11:00:20 +0530

    edge browser

---
+++ WebAutomation/src/com/sgtesting/test/EdgeBrowser.java
@@ -0,0 +1,54 @@
+package com.sgtesting.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+
+public class EdgeBrowser  {
+    public static WebDriver obrowser=null;
+	public static void main(String[] args) {
+		launchBrowser();
+		navigateURL();
+		closeApplicationURL();
+
+	}
+	
+	private static void launchBrowser()
+	{
+		try
+		{
+			obrowser=new EdgeDriver();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	private static void navigateURL()
+	{
+		try
+		{
+			obrowser.get("https://cetonline.karnataka.gov.in/kea/");
+			Thread.sleep(5000);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	private static void closeApplicationURL()
+	{
+		try
+		{
+			obrowser.quit();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+}
+
